707103476fb0b7fafc102f0b4ed16e4ae4bb7368
David Potentini <davidzardo2016@gmail.com>
2026-08-17 19:35:00 -0300

    chore(backend): maven wrapper, gitattributes e teste inicial

---
+++ backend/src/test/java/com/github/davidpotentini/CerneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.davidpotentini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CerneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
